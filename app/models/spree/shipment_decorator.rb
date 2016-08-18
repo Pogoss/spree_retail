f7a87c7603bb72369ab79f2ded1d693bc246b242
vova <Surgeon1989@gmail.com>
2016-08-18 18:26:48 +0300

    add file output on update order test

--- app/models/spree/shipment_decorator.rb
+++ app/models/spree/shipment_decorator.rb
@@ -1,13 +1,13 @@
 module Spree
   Spree::Shipment.class_eval do
     after_create :send_spree_order_update, unless: :retail_update
-    after_update :send_spree_order_update, unless: :retail_update
+    before_update :send_spree_order_update#, unless: :retail_update
 
     attr_accessor :retail_update
 
     def send_spree_order_update
       if order
-        File.open('public/retail.txt', 'a') { |file| file.write("#{retail_update.to_s} shipment\n") }
+        File.open('public/retail.txt', 'a') { |file| file.write("#{changes.to_s} shipment\n") }
         order.spree_send_updated
       end
     end
