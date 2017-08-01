5fbbb23336a5694e80f9ed9dc3d934461d6f67cc
Novik <novik65@gmail.com>
2017-08-01 18:44:56 +0300

    minor fix

--- app/models/spree/shipment_decorator.rb
+++ app/models/spree/shipment_decorator.rb
@@ -1,6 +1,6 @@
 module Spree
   Shipment.class_eval do
-#    after_save :send_spree_order_update, unless: :retail_update
+    after_save :send_spree_order_update, unless: :retail_update
 
     attr_accessor :retail_update
 
